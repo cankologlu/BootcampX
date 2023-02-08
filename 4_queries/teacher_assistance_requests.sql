df240b25c12ba04cac8b125e8e0be2831f438213
Can Kologlu <mckologlu@gmail.com>
2023-02-07 20:41:48 -0500

    Add assistance queries

---
+++ 4_queries/teacher_assistance_requests.sql
@@ -0,0 +1,4 @@
+SELECT count(assistance_requests.*) as total_assistance, teachers.name as name FROM assistance_requests
+JOIN teachers ON teacher_id = teachers.id 
+WHERE name = 'Waylon Boehm'
+GROUP BY name;
