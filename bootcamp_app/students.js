e3600b4696e8e03daacf066350e2c2f5c3c9175e
Can Kologlu <mckologlu@gmail.com>
2023-02-09 13:26:37 -0500

    parametrized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,14 +9,20 @@ const pool = new Pool({
 
 const incoming = process.argv.slice(2)
 
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${incoming[0]}%'
-LIMIT ${incoming[1] || 5};
-`)
+
+// Store all potentially malicious values in an array.
+const values = [`%${incoming[0]}%`, (incoming[1] || 5)];
+
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,17 +8,18 @@ const pool = new Pool({
 });
 
 const incoming = process.argv.slice(2)
-
-
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${incoming[0]}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`
+const values = [(`%${incoming[0]}%` || 'FEB')]
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
